58a7f6219e32af48fb7dd349b69d93cb4dd82b10
Akshay Ravikumar <akshay.ravikumar@rubrik.com>
2019-06-28 16:48:33 -0700

    hmm

---
+++ assets/js/problems.js
@@ -0,0 +1,17 @@
+let problems = [
+	{
+		"title": "Quadratic Formula",
+		"description": "Classic.",
+		"latex": String.raw`x = \dfrac{-b\pm\sqrt{b^2-4ac}}{2a}`
+	},
+	{
+		"title": "Pythagorean Theorem",
+		"description": "Classic.",
+		"latex": String.raw`c = \sqrt{a^2+b^2}`
+	},
+	{
+		"title": "Sum of Squares",
+		"description": "Classic.",
+		"latex": String.raw`\sum_{i=1}^n i^2 = \frac{n(n+1)(2n+1)}{6}`
+	}
+];
