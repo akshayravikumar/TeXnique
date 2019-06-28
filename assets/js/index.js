94d3a4a85e2b6146c07aa5a89bf191c048ef6676
Akshay Ravikumar <akshay.ravikumar@rubrik.com>
2019-06-28 16:48:33 -0700

    hmm

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-    var target = String.raw`c = \sqrt{a^2 + b^2}`;
-    katex.render(target, $("#target")[0], {
+    let target = problems[Math.floor(Math.random()*problems.length)];
+    katex.render(target.latex, $("#target")[0], {
         throwOnError: false
     });
 
-    var oldVal = "";
+    let oldVal = "";
     $("#user-input").on("change keyup paste", function() {
-        var currentVal = $(this).val();
+        let currentVal = $(this).val();
         if(currentVal == oldVal) {
             return; //check to prevent multiple simultaneous triggers
         }
@@ -19,14 +19,14 @@ $(document).ready(function() {
 
         html2canvas($('#target'),{
             onrendered: function (targetCanvas) {  
-                var width = targetCanvas.width;
-                var height = targetCanvas.height;
-                var targetData = targetCanvas.getContext("2d").getImageData(0, 0, width, height);
+                let width = targetCanvas.width;
+                let height = targetCanvas.height;
+                let targetData = targetCanvas.getContext("2d").getImageData(0, 0, width, height);
                 html2canvas($('#out'),{
                     onrendered: function (outCanvas) {  
-                        var outData = outCanvas.getContext("2d").getImageData(0, 0, width, height); 
-                        var diff = pixelmatch(targetData.data, outData.data, undefined, width, height, {threshold: 0.1});
-                        var result = "";
+                        let outData = outCanvas.getContext("2d").getImageData(0, 0, width, height); 
+                        let diff = pixelmatch(targetData.data, outData.data, undefined, width, height, {threshold: 0.1});
+                        let result = "";
                         if (diff < 10) {
                             result = "Nice! :D";
                         } else {
