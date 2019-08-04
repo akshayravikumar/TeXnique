6100d8d7e985a4dc8df999a29b7de48128f35fe1
Rahul Sridhar <sridhar.rahul@gmail.com>
2019-08-03 19:28:23 -0700

    Some marginally improved styling

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,40 +1,45 @@
 $(document).ready(function() {
     let target = problems[Math.floor(Math.random()*problems.length)];
+    // let target = problems[problems.length-1];
     katex.render(target.latex, $("#target")[0], {
-        throwOnError: false
+        throwOnError: false,
+        displayMode: true
     });
 
     let oldVal = "";
     $("#user-input").on("change keyup paste", function() {
         let currentVal = $(this).val();
         if(currentVal == oldVal) {
-            return; //check to prevent multiple simultaneous triggers
+            return; // check to prevent multiple simultaneous triggers
         }
 
         oldVal = currentVal;
-        //action to be performed on textarea changed
+        // action to be performed on textarea changed
         katex.render(currentVal, $("#out")[0], {
-            throwOnError: false
+            throwOnError: false,
+            displayMode: true
         });
 
         html2canvas($('#target'),{
-            onrendered: function (targetCanvas) {  
+            onrendered: function (targetCanvas) {
+                $('#out').parent().removeClass("correct");
                 let width = targetCanvas.width;
                 let height = targetCanvas.height;
                 let targetData = targetCanvas.getContext("2d").getImageData(0, 0, width, height);
                 html2canvas($('#out'),{
-                    onrendered: function (outCanvas) {  
-                        let outData = outCanvas.getContext("2d").getImageData(0, 0, width, height); 
+                    onrendered: function (outCanvas) {
+                        let outData = outCanvas.getContext("2d").getImageData(0, 0, width, height);
                         let diff = pixelmatch(targetData.data, outData.data, undefined, width, height, {threshold: 0.1});
                         let result = "";
                         if (diff < 10) {
                             result = "Nice! :D";
+                            $('#out').parent().addClass("correct");
                         } else {
                             result = "Rip. :("
                         }
-                        $("#result").text(result);
+                        // $("#result").text(result);
                     }
-                });      
+                });
             }
         });
 
